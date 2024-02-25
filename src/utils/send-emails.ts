786604e863fe5e92b1a9ceb1ec524ec2b1077f87
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-25 15:58:53 +0700

    feat: refactor sendEmail function to use environment variables for sender and email address

--- src/utils/send-emails.ts
+++ src/utils/send-emails.ts
@@ -1,16 +1,20 @@
 import { Resend } from 'resend';
 
+import { RESEND_EMAIL, RESEND_SENDER } from '../config';
+
 const resend = new Resend(process.env.RESEND_KEY);
 
 export const sendEmail = async ({ to, subject, text }: { to: string[]; subject: string; text: string }) => {
     const { error } = await resend.emails.send({
-        from: 'JKT48 Private Message <hello@fleetime.my.id>',
+        from: `${RESEND_SENDER} <${RESEND_EMAIL}>`,
         to,
         subject,
         text,
     });
 
     if (error) {
-        throw new Error(`Failed to send email: ${error.message}`);
+        return { success: false, error: `Failed to send email: ${error.message}` };
     }
+
+    return { success: true };
 };
