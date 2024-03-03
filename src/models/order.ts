a8ce9142823cc746623c694df1ffca21d1cba0ad
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-03 07:45:05 +0700

    feat: add order model definition

---
+++ src/models/order.ts
@@ -0,0 +1,31 @@
+import { numeric, pgEnum, pgTable, uuid } from 'drizzle-orm/pg-core';
+
+import { packagePayment } from './package';
+import { users } from './users';
+
+export const oderStatusEnum = pgEnum('order_status', ['pending', 'success', 'failed']);
+
+export const paymentMethodEnum = pgEnum('payment_method', [
+    'xendit',
+    'midtrans',
+    'gopay',
+    'ovo',
+    'dana',
+    'google_pay',
+    'apple_pay',
+]);
+
+export const order = pgTable('order', {
+    id: uuid('id').primaryKey().unique().notNull().defaultRandom(),
+    userId: uuid('user_id')
+        .notNull()
+        .references(() => users.id),
+    packageId: uuid('package_id')
+        .notNull()
+        .references(() => packagePayment.id),
+    paymentMethod: paymentMethodEnum('payment_method'),
+    subtotal: numeric('subtotal').notNull(),
+    tax: numeric('tax').notNull(),
+    total: numeric('total').notNull(),
+    orderStatus: oderStatusEnum('order_status'),
+});
