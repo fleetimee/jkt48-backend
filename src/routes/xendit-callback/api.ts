a0e7c4e28bfaebbc73866f4c50450f02af31a0a1
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-06 15:35:18 +0700

    feat: refactor Xendit callback API endpoint

--- src/routes/xendit-callback/api.ts
+++ src/routes/xendit-callback/api.ts
@@ -1,25 +1,37 @@
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 import { updateOrderStatusXenditCallback } from './repository';
 
 const router = express.Router();
 
-router.post('/xendit-callback', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     try {
         const { body } = req;
+
+        // Check if body is empty
+        if (!body) {
+            res.status(StatusCodes.BAD_REQUEST).send({
+                message: 'Invalid body',
+            });
+        }
 
         if (body.status === 'PAID') {
             console.log('Xendit callback:', body);
 
             await updateOrderStatusXenditCallback(body.external_id, 'success');
+
+            res.status(StatusCodes.OK).send({
+                message: 'Xendit callback received',
+            });
         } else if (body.status === 'FAILED') {
             console.log('Xendit callback:', body);
 
             await updateOrderStatusXenditCallback(body.external_id, 'failed');
         }
 
-        res.status(200).send({
-            message: 'Xendit callback received',
+        res.status(StatusCodes.BAD_REQUEST).send({
+            message: 'Invalid status',
         });
     } catch (error) {
         next(error);
