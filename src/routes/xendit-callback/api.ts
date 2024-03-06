a4ca76b34f05796110da4eb4e4922b7dda0f063f
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-06 15:17:03 +0700

    feat: add Xendit callback API endpoint

---
+++ src/routes/xendit-callback/api.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import { updateOrderStatusXenditCallback } from './repository';
+
+const router = express.Router();
+
+router.post('/xendit-callback', async (req, res, next) => {
+    try {
+        const { body } = req;
+
+        if (body.status === 'PAID') {
+            console.log('Xendit callback:', body);
+
+            await updateOrderStatusXenditCallback(body.external_id, 'success');
+        } else if (body.status === 'FAILED') {
+            console.log('Xendit callback:', body);
+
+            await updateOrderStatusXenditCallback(body.external_id, 'failed');
+        }
+
+        res.status(200).send({
+            message: 'Xendit callback received',
+        });
+    } catch (error) {
+        next(error);
+    }
+});
