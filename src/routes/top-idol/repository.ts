37fa7441d85287bf79ab5e0f834c2d46d7066c99
Syafrizal Muhammad <muhammadsyafr@gmail.com>
2024-03-08 15:25:03 +0700

    feat: add api and cron to fill the table idol_top

--- src/routes/top-idol/repository.ts
+++ src/routes/top-idol/repository.ts
@@ -24,3 +24,52 @@ export const getTopIdol = async () => {
 
     return topIdol;
 };
+
+/**
+ * Retrieves the top idol data by 7 last day.
+ * @returns {Promise<Array<Object>>} The top idol data.
+ */
+export const getTopIdolByOrderTransaction = async () => {
+    const topIdolCount = await db.execute(sql`
+    SELECT idol_id, COUNT(idol_id) AS idol_count
+    FROM order_idol
+    WHERE order_id IN (
+        SELECT id
+        FROM "order"
+        WHERE created_at BETWEEN CURRENT_DATE - INTERVAL '7 days' AND CURRENT_DATE AND order_status = 'success'
+    )
+    GROUP BY idol_id;
+    `);
+    return topIdolCount;
+};
+
+/**
+ * Truncate table idol_top.
+ * @returns {Boolean}
+ */
+export const trunctateTopIdols = async () => {
+    const truncateTopIdol = await db.execute(sql`
+        TRUNCATE TABLE idol_top;
+    `);
+
+    if (truncateTopIdol) return true;
+};
+
+/**
+ * Post the top idol data.
+ * @returns {Promise<Array<Object>>} The top idol data.
+ */
+interface Datas {
+    id_idol: string;
+    subscription_count: string;
+}
+export const storeTopIdols = async (datas: Datas[]) => {
+    await db.transaction(async trx => {
+        for (const idol of datas) {
+            await trx.execute(sql`
+                INSERT INTO idol_top (id_idol, subscription_count)
+                VALUES (${idol.id_idol}, ${idol.subscription_count}) RETURNING id_idol, subscription_count;
+            `);
+        }
+    });
+};
