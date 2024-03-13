5d6582d6eb358fab3266c5787bc2ddf62a5aa583
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-13 07:34:49 +0700

    feat: refactor authentication route in auth/spec.ts

--- src/routes/auth/spec.ts
+++ src/routes/auth/spec.ts
@@ -24,89 +24,47 @@
  *
  * @swagger
  * /api/login:
-
  *   post:
-
  *     summary: Authenticate user to get access token
-
  *     tags:
-
  *       - Auth
-
  *     requestBody:
-
  *       required: true
-
  *       content:
-
  *         application/json:
-
  *           schema:
-
  *             $ref: "#/components/schemas/Login"
-
  *     responses:
-
  *       "200":
-
  *         description: Upon successful login
-
  *         content:
-
  *           application/json:
-
  *             schema:
-
  *               type: object
-
  *               properties:
-
  *                 accessToken:
-
  *                   type: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
-
  *                   description: Access token to be used for subsequent requests
-
  *               example:
-
  *                 accessToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
  *       "401":
-
  *         description: Wrong credentials
-
  *         content:
-
  *           application/json:
-
  *             schema:
-
  *               type: object
-
  *               properties:
-
  *                 error:
-
  *                   type: string
-
  *                   description: Error message
-
  *       "403":
-
  *         description: Email not yet verified, could not login
-
  *         content:
-
  *           application/json:
-
  *             schema:
-
  *               type: object
-
  *               properties:
-
  *                 error:
-
  *                   type: string
-
  *                   description: Error message
  */
