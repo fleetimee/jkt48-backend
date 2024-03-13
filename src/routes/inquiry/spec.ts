bf191d483215f609d2627b0d0ba099ff8f16df70
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-13 11:15:54 +0700

    feat: add inquiry route with Swagger documentation

---
+++ src/routes/inquiry/spec.ts
@@ -0,0 +1,43 @@
+/**
+ * @swagger
+ * /api/inquiry/{id}:
+ *   get:
+ *     summary: Retrieve inquiries by package ID
+ *     tags: [Inquiries]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The package ID
+ *     responses:
+ *       200:
+ *         description: Success fetches inquiries
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 code:
+ *                   type: integer
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Inquiry'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
