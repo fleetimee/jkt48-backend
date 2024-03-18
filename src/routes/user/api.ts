58d4a669c5466e06ecb96a6177e621facc312f4b
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-18 17:03:26 +0700

    feat: console.log statement and fix variable name in user API

--- src/routes/user/api.ts
+++ src/routes/user/api.ts
@@ -182,8 +182,6 @@ router.get('/me/conversation/:conversationId', authenticateUser, async (req, res
         const conversation = await getUserConversationMessages(id, conversationId, 10, 0);
         if (!conversation) throw new NotFoundError('Conversation not found');
 
-        console.log('conversation', conversation);
-
         // From the conversation list, get the first item to get idol id
         const idolId = conversation[0]?.idol_id;
 
