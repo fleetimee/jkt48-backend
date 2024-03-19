baec8b5f9558a778adae2d3407df919a9c978d71
Novian Andika <novian.andika@students.amikom.ac.id>
2024-03-19 15:40:28 +0700

    feat: refactor getAllAttachmentsByConversationId function

--- src/routes/attachment/repository.ts
+++ src/routes/attachment/repository.ts
@@ -4,14 +4,12 @@ import db from '../../db';
 import { message } from '../../models/message';
 import { attachment } from '../../models/message_attachment';
 
+/**
+ * Retrieves all attachments associated with a conversation.
+ * @param conversationId - The ID of the conversation.
+ * @returns A Promise that resolves to an array of attachments.
+ */
 export const getAllAttachmentsByConversationId = async (conversationId: string) => {
-    // const attachments = await db.execute(sql`
-    //     SELECT ma.file_path AS file_path
-    //     FROM message m
-    //             JOIN message_attachment ma ON m.id = ma.message_id
-    //     WHERE m.conversation_id = ${conversationId};
-    // `);
-
     const attachments = await db
         .select({
             filePath: attachment.filePath,
