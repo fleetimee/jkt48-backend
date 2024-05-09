e98aaf54a58173463c174798282f4a2030b58ca3
Novian Andika <novian.andika@students.amikom.ac.id>
2024-05-09 19:55:48 +0700

    refactor: improve random token generation in lib.ts

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -4,27 +4,25 @@
  * @returns The generated verification code.
  */
 export const generateVerificationCode = () => {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let verificationCode = '';
     for (let i = 0; i < 6; i++) {
-        verificationCode += chars.charAt(Math.floor(Math.random() * chars.length));
+        verificationCode += Math.floor(Math.random() * 10); // Generate a random number between 0 and 9
     }
     return verificationCode;
 };
-
 /**
  * Generates a random string consisting of alphanumeric characters.
  *
  * @returns The generated random string.
  */
 export const generateResetTokenPassword = () => {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let randomString = '';
-    for (let i = 0; i < 14; i++) {
-        randomString += chars.charAt(Math.floor(Math.random() * chars.length));
+    let resetToken = '';
+    for (let i = 0; i < 6; i++) {
+        resetToken += Math.floor(Math.random() * 10); // Generate a random number between 0 and 9
     }
-    return randomString;
+    return resetToken;
 };
+
 /**
  * Calculates the tax and total amount for a given price and tax rate.
  * @param price - The price of the item.
