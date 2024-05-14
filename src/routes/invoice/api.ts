e1c42eadad62fa80392c1d2895e1029efbac93a5
Novian Andika <novian.andika@students.amikom.ac.id>
2024-05-15 05:59:56 +0700

    feat: create scheduled invoice for close to expiration orders

--- src/routes/invoice/api.ts
+++ src/routes/invoice/api.ts
@@ -7,11 +7,91 @@ import { validateSchema } from '../../middlewares/validate-request';
 import { UnprocessableEntityError } from '../../utils/errors';
 import { formatResponse } from '../../utils/response-formatter';
 import { validateUuid } from '../../utils/validate';
-import { getInquiryOrder, getOrderById } from '../order/repository';
+import { getCloseToExpirationOrders, getInquiryOrder, getOrderById } from '../order/repository';
+import { getUserById, getUserPhoneNumber } from '../user/repository';
 import { createInvoice, expireInvoice, getInvoice, getInvoices } from './repository';
 import { createInvoiceSchema } from './schema';
 
 const router = express.Router();
+
+router.get('/scheduledInvoice', async (req, res, next) => {
+    try {
+        // Get orderId that almost expired
+        const orderIds = await getCloseToExpirationOrders();
+
+        if (!orderIds || orderIds.length === 0) {
+            res.status(StatusCodes.OK).send({
+                success: true,
+                code: StatusCodes.OK,
+                message: 'No order that almost expired',
+                data: null,
+            });
+            return;
+        }
+
+        for (const orderId of orderIds) {
+            console.log(orderId);
+
+            // Get order details
+            const order = await getOrderById(orderId.id as unknown as string);
+            console.log(order);
+
+            // Get user from order
+            const user = await getUserById(order.userId);
+            console.log(user);
+
+            // Inquiry order to get package details
+            const inquiryOrder = await getInquiryOrder(orderId.id as unknown as string);
+            console.log(inquiryOrder);
+
+            // Build request body for creating invoice
+            const invoice = await createInvoice({
+                externalId: orderId.id as unknown as string,
+                payerEmail: user.email,
+                amount: inquiryOrder.order_total as number,
+                currency: 'IDR',
+                description: `Pembayaran untuk JKT48 Private Message - ${inquiryOrder.package_name}`,
+                local: 'id',
+                customer: {
+                    email: user.email,
+                    givenNames: user.name,
+                    mobileNumber: user.phoneNumber as string,
+                },
+                customerNotificationPreference: {
+                    invoiceCreated: ['email', 'whatsapp', 'sms'],
+                    invoicePaid: ['email', 'whatsapp', 'sms'],
+                    invoiceReminder: ['email', 'whatsapp', 'sms'],
+                },
+                items: [
+                    {
+                        name: inquiryOrder.package_name as string,
+                        price: inquiryOrder.order_subtotal as number,
+                        quantity: 1,
+                        category: inquiryOrder.category as string,
+                    },
+                ],
+                fees: [
+                    {
+                        type: 'PPN',
+                        value: inquiryOrder.order_tax as number,
+                    },
+                ],
+            });
+
+            console.log(invoice);
+
+            res.status(StatusCodes.OK).send({
+                success: true,
+                code: StatusCodes.OK,
+                message: 'Scheduled invoice created',
+                data: null,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
 
 router.get('/', authenticateUser, requireAdminRole, async (req, res, next) => {
     try {
@@ -83,6 +163,13 @@ router.post('/', validateSchema(createInvoiceSchema), authenticateUser, async (r
         const order = await getOrderById(idOrder);
         if (!order) throw new UnprocessableEntityError('The order does not exist');
 
+        let phoneNumber = await getUserPhoneNumber(id);
+
+        if (!phoneNumber) {
+            phoneNumber = { phoneNumber: null };
+        }
+        if (!phoneNumber) throw new UnprocessableEntityError('The user does not have a phone number');
+
         // Check if the order is not already paid and is within 7 days of expiration
         if (order.orderStatus === 'success') {
             const currentDate = new Date();
@@ -105,12 +192,19 @@ router.post('/', validateSchema(createInvoiceSchema), authenticateUser, async (r
             payerEmail: email,
             amount: inquiryOrder.order_total as number,
             currency,
-            description: 'Pembayaran JKT48 Private Message',
+            description: `Pembayaran untuk JKT48 Private Message - ${inquiryOrder.package_name}`,
+            local: 'id',
             customer: {
                 id: id,
                 email: email,
                 customerId: `${id}-${Date.now()}`,
                 givenNames: name,
+                mobileNumber: phoneNumber.phoneNumber as string,
+            },
+            customerNotificationPreference: {
+                invoiceCreated: ['email', 'whatsapp', 'sms'],
+                invoicePaid: ['email', 'whatsapp', 'sms'],
+                invoiceReminder: ['email', 'whatsapp', 'sms'],
             },
             items: [
                 {
@@ -119,11 +213,11 @@ router.post('/', validateSchema(createInvoiceSchema), authenticateUser, async (r
                     quantity: 1,
                     category: inquiryOrder.category as string,
                 },
+            ],
+            fees: [
                 {
-                    name: 'Tax',
-                    price: inquiryOrder.order_tax as number,
-                    quantity: 1,
-                    category: 'tax',
+                    type: 'PPN',
+                    value: inquiryOrder.order_tax as number,
                 },
             ],
         });
--- src/routes/subscription/schema.ts
+++ src/routes/subscription/schema.ts
@@ -5,5 +5,6 @@ export const createCustomerSchema = z.object({
         reference_id: z.string().min(1, 'Please enter a reference ID'),
         given_names: z.string().min(1, 'Please enter a name'),
         email: z.string().email(),
+        mobile_number: z.string().regex(/^\+?[1-9]\d{1,14}$/, 'Please enter a valid phone number in E.164 format'),
     }),
 });
