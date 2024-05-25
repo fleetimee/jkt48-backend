cd905c75329699af491576e80454caa23fe3a09c
Novian Andika <novian.andika@students.amikom.ac.id>
2024-05-25 12:21:58 +0700

    chore: update variable name in appleVerifySchema

--- src/routes/apple-pay/schema.ts
+++ src/routes/apple-pay/schema.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const appleVerifySchema = z.object({
     body: z.object({
-        receiptData: z.string().min(1, 'Please enter your receipt'),
+        signedPayload: z.string().min(1, 'Please enter your receipt'),
     }),
 });
