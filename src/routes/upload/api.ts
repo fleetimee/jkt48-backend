bda22b386c1eec049444da5857c002ad54e95528
Novian Andika <novian.andika@students.amikom.ac.id>
2024-06-18 18:16:03 +0700

    refactor: remove image compression from upload API

--- src/routes/upload/api.ts
+++ src/routes/upload/api.ts
@@ -1,29 +1,19 @@
 import express from 'express';
-import fs from 'fs';
 import { StatusCodes } from 'http-status-codes';
 import multer from 'multer';
-import sharp from 'sharp';
 
 import { upload } from '../../utils/multer';
 import { formatResponse } from '../../utils/response-formatter';
 
 const router = express.Router();
 
-router.post('/:urlParam(news|profile)', upload.single('file'), async (req, res, next) => {
+router.post('/:urlParam(news|profile)', upload.single('file'), (req, res, next) => {
     try {
         if (!req.file) {
             res.status(StatusCodes.BAD_REQUEST).send({ error: 'No file uploaded' });
             return;
         }
         const filePath = `/static/${req.params.urlParam}/${req.file.filename}`;
-
-        if (req.file.mimetype.startsWith('image/')) {
-            const tempOutputPath = `${req.file.path}_temp`; // Create a temporary output path
-            await sharp(req.file.path)
-                .jpeg({ quality: 80 }) // Convert to JPEG with 80% quality
-                .toFile(tempOutputPath); // Write to the temporary file
-            fs.renameSync(tempOutputPath, req.file.path); // Replace the original file with the temporary one
-        }
 
         res.status(StatusCodes.OK).send(
             formatResponse({
