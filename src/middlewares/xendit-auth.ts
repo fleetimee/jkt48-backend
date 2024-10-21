6fbb9a06ee313c344746037b8046616615a3ca55
Shionne Imeris <hello@fleetime.my.id>
2024-10-21 10:31:42 +0700

    feat: normalize header key for Xendit token verification and add logging

--- src/middlewares/xendit-auth.ts
+++ src/middlewares/xendit-auth.ts
@@ -4,7 +4,9 @@ import { StatusCodes } from 'http-status-codes';
 const XENDIT_TOKEN = 'yPA3KQkzCl7lPNNTrGy2dmOOwmH18FPb4P9XUole67Q0UrvA';
 
 export const verifyXenditToken = (req: Request, res: Response, next: NextFunction) => {
-    const callbackToken = req.headers['X-CALLBACK-TOKEN'];
+    const callbackToken = req.headers['x-callback-token'] as string;
+
+    console.log('callbackToken', callbackToken);
 
     if (!callbackToken || callbackToken !== XENDIT_TOKEN) {
         return res.status(StatusCodes.UNAUTHORIZED).send({
