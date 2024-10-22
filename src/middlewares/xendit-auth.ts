2659d59d4ec81c999b180c9a59e00bd034c17e30
Shionne Imeris <hello@fleetime.my.id>
2024-10-22 12:50:29 +0700

    fix: remove debug logging for callback token in Xendit authentication middleware

--- src/middlewares/xendit-auth.ts
+++ src/middlewares/xendit-auth.ts
@@ -6,8 +6,6 @@ const XENDIT_TOKEN = 'yPA3KQkzCl7lPNNTrGy2dmOOwmH18FPb4P9XUole67Q0UrvA';
 export const verifyXenditToken = (req: Request, res: Response, next: NextFunction) => {
     const callbackToken = req.headers['x-callback-token'] as string;
 
-    console.log('callbackToken', callbackToken);
-
     if (!callbackToken || callbackToken !== XENDIT_TOKEN) {
         return res.status(StatusCodes.UNAUTHORIZED).send({
             message: 'Unauthorized',
