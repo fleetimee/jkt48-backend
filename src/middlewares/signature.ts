e1af7624935a9226bfb76699e8e61e590ee2fa8b
Shionne Imeris <hello@fleetime.my.id>
2024-10-27 07:51:54 +0700

    feat: simplify signature validation middleware

--- src/middlewares/signature.ts
+++ src/middlewares/signature.ts
@@ -5,38 +5,29 @@ import { redisClient } from './caching';
 
 const SERVER_CLIENT_SECRET = '6ozHw3Q53W8c8U9cEDKUf6BEi2hgEz5j';
 
-// Helper function to remove problematic characters from request body
 const sanitizeBody = (body: string): string => {
-    return body.replace(/[\n\r]/g, '').trim(); // Removes newline, carriage returns, and trims whitespace
+    return body.replace(/[\n\r]/g, '').trim();
 };
 
 const validateSignatureMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const apiClientSecret = req.headers['api-client-secret'] as string;
     const signature = req.headers['signature-mitra'] as string;
     const timestamp = req.headers['timestamp-mitra'] as string;
-    const nonce = req.headers['nonce'] as string; // New nonce header
+    const nonce = req.headers['nonce'] as string;
     const httpMethod = req.method;
 
-    if (!apiClientSecret || !signature || !timestamp || !nonce) {
+    if (!signature || !timestamp || !nonce) {
         return res.status(400).json({ message: 'Missing required headers' });
     }
 
-    if (apiClientSecret !== SERVER_CLIENT_SECRET) {
-        return res.status(403).json({ message: 'Invalid client secret' });
-    }
-
-    // Check if signature (nonce-timestamp pair) was used
     const signatureKey = `signature:${timestamp}:${nonce}`;
     const isUsed = await redisClient.exists(signatureKey);
     if (isUsed) {
         return res.status(403).json({ message: 'Signature already used' });
     }
 
-    // Handle and sanitize request body
     let requestBody = req.method !== 'GET' ? JSON.stringify(req.body) : '';
     requestBody = sanitizeBody(requestBody);
 
-    // Construct the payload to generate HMAC
     const payload = `verb=${httpMethod}&timestamp=${timestamp}&nonce=${nonce}&body=${requestBody}`;
 
     const hmac = CryptoJS.HmacSHA256(payload, SERVER_CLIENT_SECRET);
@@ -46,8 +37,7 @@ const validateSignatureMiddleware = async (req: Request, res: Response, next: Ne
         return res.status(403).json({ message: 'Invalid signature' });
     }
 
-    // Store the signature as used with expiration (e.g., 5 minutes)
-    await redisClient.set(signatureKey, 'used', 'EX', 300); // Expire in 300 seconds (5 minutes)
+    await redisClient.set(signatureKey, 'used', 'EX', 300);
 
     next();
 };
